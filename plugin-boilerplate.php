0e07094676580e854d93c034e5397f26352e750a
Fahad <fahad.abdullah1280@gmail.com>
2022-04-18 02:07:23 +0600

    activator and deactivator added

--- plugin-boilerplate.php
+++ plugin-boilerplate.php
@@ -17,10 +17,17 @@ if ( ! defined( 'WPINC' ) ) {
 class PluginBoilerplate {
     function __construct(){
         add_action( 'plugins_loaded', array( self::class,'pb_load_textdomain' ) );
+        register_activation_hook( __FILE__, array( self::class, 'pb_activate' ) );
     }
 
     private function pb_load_textdomain() {
-        load_plugin_textdomain( 'se-mcq', false, plugin_dir_path( __FILE__ ) . "languages/" );
+        load_plugin_textdomain( 'fa-plugin-boilerplate', false, plugin_dir_path( __FILE__ ) . "languages/" );
+    }
+
+    private function pb_activate(){
+        // Do something on plugin activation
+        require_once plugin_dir_path( __FILE__ ) . 'includes/class-pb-activator.php';
+        PluginBoilerplateActivator::activate();
     }
 }
 new PluginBoilerplate();
