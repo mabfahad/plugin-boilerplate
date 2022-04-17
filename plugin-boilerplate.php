d29728debf9ea14ad17ac4b931d1c5ced1598368
Fahad <fahad.abdullah1280@gmail.com>
2022-04-18 02:11:36 +0600

    class structured

--- plugin-boilerplate.php
+++ plugin-boilerplate.php
@@ -13,21 +13,58 @@
 if ( ! defined( 'WPINC' ) ) {
     die;
 }
+//Define Dirpath for hooks
+define( 'DIR_PATH', plugin_dir_path( __FILE__ ) );
 
-class PluginBoilerplate {
-    function __construct(){
-        add_action( 'plugins_loaded', array( self::class,'pb_load_textdomain' ) );
-        register_activation_hook( __FILE__, array( self::class, 'pb_activate' ) );
-    }
+if (!class_exists('PluginBoilerplate')) {
+    class PluginBoilerplate
+    {
+        /**
+         * PluginBoilerplate constructor.
+         */
+        public function __construct()
+        {
+            //Hook into the plugin activation hook
+            register_activation_hook( __FILE__, array( $this, 'activate' ) );
+            //Hook into the plugin deactivation hook
+            register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+            //Hook into the plugin uninstall hook
+            register_uninstall_hook( __FILE__, array( $this, 'uninstall' ) );
+            //Hook into the WordPress init action
+            add_action( 'init', array( $this, 'init' ) );
+        }
 
-    private function pb_load_textdomain() {
-        load_plugin_textdomain( 'fa-plugin-boilerplate', false, plugin_dir_path( __FILE__ ) . "languages/" );
-    }
+        /**
+         * Activate the plugin
+         */
+        public function activate()
+        {
+            // Do nothing
+        }
 
-    private function pb_activate(){
-        // Do something on plugin activation
-        require_once plugin_dir_path( __FILE__ ) . 'includes/class-pb-activator.php';
-        PluginBoilerplateActivator::activate();
+        /**
+         * Deactivate the plugin
+         */
+        public function deactivate()
+        {
+            // Do nothing
+        }
+
+        /**
+         * Uninstall the plugin
+         */
+        public function uninstall()
+        {
+            // Do nothing
+        }
+
+        /**
+         * Initialize the plugin
+         */
+        public function init()
+        {
+            // Do nothing
+        }
     }
 }
 new PluginBoilerplate();
